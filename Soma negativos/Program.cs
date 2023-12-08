bd619fb17448bf13110b1761080d3193a07a4e2c
Tony <antony.branquinho@gmail.com>
2023-12-08 12:44:18 -0300

    Legal

---
+++ Soma negativos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soma_negativos {
+    class Program {
+        static void Main(string[] args) {
+
+            int m, n, i, j;
+
+            Console.Write("Quantidade de linhas da matriz? ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Quantidade de colunas da matriz? ");
+            m = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, m];
+
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < m; j++) {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("VALORES NEGATIVOS:");
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < m; j++) {
+                    if (matriz[i, j] < 0) {
+                        Console.WriteLine(matriz[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
